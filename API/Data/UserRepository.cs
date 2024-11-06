f910ff01bc255fe7b641bc5049a7d925ce6e036c
Sheldon <lhanco@gmail.com>
2024-11-06 12:33:00 +0700

    13_Add Likes feature and show Likes with pagination.

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -49,7 +49,7 @@ public class UserRepository(DataContext context, IMapper mapper) : IUserReposito
         };
 
         return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(mapper.ConfigurationProvider), 
-            userParams.pageNumber, userParams.PageSize);
+            userParams.PageNumber, userParams.PageSize);
     }
 
     public async Task<AppUser?> GetUserByIdAsync(int id)
--- client/src/app/list/list.component.ts
+++ client/src/app/list/list.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { LikesService } from '../_services/likes.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, MemberCardComponent, PaginationModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export class ListComponent implements OnInit, OnDestroy {
+  likesService = inject(LikesService);
+  predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);    
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+  }
+
+  getTitle() {
+    switch (this.predicate) {
+      case 'liked': return 'Members you liked';
+      case 'likedBy': return 'Members who liked you';
+      default: return 'Mutual';
+    }
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
 }
